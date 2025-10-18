b71fcc2dc8e66539a2baab2eaa17e8740bf189e6
eliezer o grande dev <eliezerassuncaocustodio@hotmail.com>
2025-10-18 20:41:43 -0300

    external-references (was added reference for the fonts undertale and genshin impact) and tested the fonts

--- external-references.lua
+++ external-references.lua
@@ -1,3 +1,7 @@
 local refs = {
     ["Wallpaper"] = "https://wall.alphacoders.com/big.php?i=1291095"
+    ["Fonts"] = {
+        ["Undertale"] = "https://www.dafontfree.io/undertale-logo-font/",
+        ["GenshinImpact"] = "https://www.dafontfree.io/genshin-impact-font/",
+    }
 }
